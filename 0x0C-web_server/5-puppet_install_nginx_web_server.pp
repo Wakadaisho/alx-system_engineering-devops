b5612b71232c341afbd1032e980129e2a8f8bc9b
Wakadaisho <paulwahome9@gmail.com>
2023-08-29 09:06:44 +0300

    Change sed replacement variables

--- 0x0C-web_server/5-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/5-puppet_install_nginx_web_server.pp
@@ -4,10 +4,10 @@ package {'nginx':
   ensure => present
 }
 
-file_line {"Redirect Me":
-  path  => '/etc/nginx/sites-available/default',
-  after => 'server \{',
-  line  => 'location /redirect_me {\n return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n }\n'
+file {'/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!
+'
 }
 
 service {'nginx':
