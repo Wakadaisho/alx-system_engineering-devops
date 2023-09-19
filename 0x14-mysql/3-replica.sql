6b04d34fcf7fb5fcf714e5bc5793f8cc61c63722
Wakadaisho <paulwahome9@gmail.com>
2023-09-19 18:20:13 +0300

    Add db backup

--- 0x14-mysql/3-replica.sql
+++ 0x14-mysql/3-replica.sql
@@ -20,5 +20,5 @@ VALUES
 GRANT SELECT ON `tyrell_corp`.`nexus6` TO 'holberton_user'@'localhost';
 
 CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'replica@mysql!pass';
-GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replica_user'@'%' IDENTIFIED BY 'replica@mysql!pass';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%' IDENTIFIED BY 'replica@mysql!pass';
 GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
