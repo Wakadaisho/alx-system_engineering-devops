da2277348e849a2bbc57fd0de536a76d2314a44b
Wakadaisho <paulwahome9@gmail.com>
2023-10-12 11:11:01 +0300

    Remove service restart

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,13 +2,5 @@
 
 # correct phpp typo
 exec {'fix_typo_in_wordpress_config':
-  command     => '/bin/sed -i s/phpp/php/g /var/www/html/*.php',
-  refreshonly => true
-}
-
-# Service resource to ensure apache2 is restarted and running
-service {'apache2':
-  ensure  => running,
-  enable  => true,
-  require => Exec['fix_typo_in_wordpress_config']
+  command     => '/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 }
